9bed1bc890ebfe15aa09b961442f2a0062535b9f
PorcusorulMagic <98654600+PorcusorulMagic@users.noreply.github.com>
2023-11-26 20:29:56 +0200

    Add files via upload

---
+++ webserver/inc/config.php
@@ -0,0 +1,59 @@
+<?php 
+// Basic Settings -----------------------------------------------------------
+
+
+// Set your site title
+$siteTitle = "Time played";
+
+// Set your site logo - "" blank for no background image
+$siteLogo = "img/cs2.png";
+
+// Set your background image - "" blank for no background image
+$background = "img/background.jpg"; 
+
+// Users per page
+$usersPerPage = "20";
+
+// Minimum total time a players needs to get in list
+$k4times_minSec = "10";
+
+
+// Set bootstrap theme - remove // infront of a theme and add // to remove old
+
+		 $stylesheet = "css/cyborg.css"; // https://github.com/StrutTower/bootsteam-theme
+
+//		 $stylesheet = "css/bootstrap.css"; // http://getbootstrap.com/
+//		 $stylesheet = "css/superhero.css"; // https://bootswatch.com/superhero/#tables
+//		 $stylesheet = "css/cosmo.css"; // https://bootswatch.com/cosmo/#tables
+//		 $stylesheet = "css/cerulean.css"; // https://bootswatch.com/cerulean/#tables
+//		 $stylesheet = "css/sandstone.css"; // https://bootswatch.com/sandstone/#tables
+//		 $stylesheet = "css/united.css"; // https://bootswatch.com/united/#tables
+//		 $stylesheet = "css/cyborg.css"; // https://bootswatch.com/cyborg/#tables
+//		 $stylesheet = "css/darkly.css"; // https://bootswatch.com/darkly/#tables
+//		 $stylesheet = "css/flatly.css"; // https://bootswatch.com/flatly/#tables
+//		 $stylesheet = "css/slate.css"; // https://bootswatch.com/slate/#tables
+//		 $stylesheet = "css/solar.css"; // https://bootswatch.com/solar/#tables
+
+
+//Time Format
+$time_format = array('Days', 'Hrs', 'Min', 'Sec');
+
+
+// MySQL Settings ------------------------------------------------------------------
+
+
+// DB hostname
+$k4times_dbHost = "localhost";
+
+// DB username
+$k4times_dbUser = "";
+
+// DB password
+$k4times_dbPass = "";
+
+// DB name
+$k4times_dbName = "";
+
+
+// End Of Settings ----------------------------------------------------------------
+?>
---
+++ webserver/inc/function.php
@@ -0,0 +1,97 @@
+<?php
+//  This file is part of MostActive sourcemod plugin.
+//
+//  Copyright (C) 2017 MostActive Dev Team
+//  https://github.com/Franc1sco/MostActive/graphs/contributors
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, per version 3 of the License.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+//  This file is based off work(s) covered by the following copyright(s):
+//
+//   Stamm 2.28 Final
+//   Copyright (C) 2012-2014 David <popoklopsi> Ordnung
+//   Licensed under GNU GPL version 3, or later.
+//   Page: https://github.com/popoklopsi/Stamm-Webinterface &
+//         https://forums.alliedmods.net/showthread.php?p=1338942
+//
+// *************************************************************************
+
+
+function secondsToTime($seconds, $time_format) {
+	date_default_timezone_set('Europe/Bucharest');
+	$dtF = new DateTime("@0");
+	$dtT = new DateTime("@$seconds");
+	$diff = $dtF->diff($dtT);
+
+	$out = array();
+	if ($diff->format('%a') > 0) $out[] = $diff->format('%a<font size="1">').' '.$time_format[0];
+	if ($diff->format('%h') > 0) $out[] = $diff->format('%h<font size="1">').' '.$time_format[1];
+	if ($diff->format('%i') > 0) $out[] = $diff->format('%i<font size="1">').' '.$time_format[2];
+	if ($diff->format('%s') > 0 || sizeof($out) == 0) {
+		$out[] = $diff->format('%s<font size="1">').' '.$time_format[3];
+	}
+	return implode(', </font>', $out);
+}
+
+class SQL
+{
+	// link identifier
+	private $db = NULL;
+
+	// Constructor
+	function __construct($host, $user, $pass, $dbName)
+	{
+		// Connect to MySQL
+		$this->db = mysqli_connect($host, $user, $pass, $dbName);
+
+		if (!$this->db) {
+			die("Couldn't make connection.");
+		}
+		mysqli_set_charset($this->db, "utf8");
+	}
+
+	// Escapes a string
+	public function escape($string)
+	{
+		return mysqli_real_escape_string($this->db, $string);
+	}
+
+	// Do a query
+	public function query($query)
+	{
+		$result = mysqli_query($this->db, $query);
+
+		if (!$result) {
+			die(mysqli_sqlstate($this->db));
+		}
+		return $result;
+	}
+
+	// Check if we found Data
+	public function foundData($result)
+	{
+		return mysqli_num_rows($result);
+	}
+
+	// Get Rows
+	public function getRows($result)
+	{
+		return mysqli_fetch_row($result);
+	}
+
+	// Get Array
+	public function getArray($result)
+	{
+		return mysqli_fetch_assoc($result);
+	}
+}
